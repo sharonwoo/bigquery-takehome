feb9b62b7c088374cf1ce0f6b3abfb749860dc69
s <sharon.woo@outlook.com>
2023-04-22 11:24:12 +0800

    dont forget the test names in test class urgh

--- test_questions.py
+++ test_questions.py
@@ -2,13 +2,13 @@ from google.cloud import bigquery
 client = bigquery.Client()
 
 class TestClass:
-    def hello_world(self):
+    def test_hello_world(self):
         x = "hello world"
         assert "hello" in x
 
     question_1 = client.query("""SELECT * FROM foodpanda-de-test-sharon.staging.question_1""")
     question_1.result()
 
-    def question_1_table_length(self, query_job=question_1): 
+    def test_question_1_table_length(self, query_job=question_1): 
         dict_query = dict(question_1)
         assert len(dict_query) == 5
