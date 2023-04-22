f8cbbce5a44690f0fe48938f665586485c70f975
s <sharon.woo@outlook.com>
2023-04-22 18:11:08 +0800

    update readme, rename dev branch workflow

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # bigquery-takehome
 make pink home again (now with rudimentary cicd)
+
+| Requirement   | Design choice        |
+| ---           | ---                  |
+| complete this exercise in Python | [use Python client](https://cloud.google.com/python/docs/reference/bigquery/latest), use the boilerplate code without refactoring first although not strictly required (e.g. for table_id, job_configs) |
+| create a public Github repository to store any code and your progress | use Github Actions to create tables in `staging` and `production` |
+| repository should also indicate how any other user can use it | TBC - plan is to have any user submit a PR to create a table in staging, and then only @sharonwoo can approve PRs to create tables in production. Also going to DRY out code so it can be used locally by anyone else |
+| Q1-Q3: create a script which follows good software engineering practices that creates a BigQuery table in your personal Google Cloud Project | script uses Github Actions to write a staging table upon push to any `dev/*` branch updating a question script python file | 
--- questions/question_1_script.py
+++ questions/question_1_script.py
@@ -1,23 +1,6 @@
 from google.cloud import bigquery
 
 '''
-Requirement:    complete this exercise in Python
-Design choice:  use Python client, https://cloud.google.com/python/docs/reference/bigquery/latest
-                source code to figure out how to write tests later: https://github.com/googleapis/python-bigquery
-                use the boilerplate code without refactoring first; can create utility functions and classes later if time/ocd permits although not strictly required (e.g. for table_id, job_configs)
-                also need to figure out linting if time permits
-
-Requirement:    create a public Github repository to store any code and your progress
-Design choice:  use Github Actions to create tables
-
-Requirement:    repository should also indicate how any other user can use it
-Design choice:  TBC - plan is to have any user submit a PR to create a table in staging, and then only @sharonwoo can approve PRs to create tables in production
-
-Requirement:    create a script which follows good software engineering practices that creates a BigQuery table in your personal Google Cloud Project
-Design choice:  this script uses Github Actions to write the table upon push to dev/*
-
-*_______*
-
 Question:       What are the 5 nearest ports to Singapore's JURONG ISLAND port? (country = 'SG', port_name = 'JURONG ISLAND')
                 Your answer should include the columns port_name and distance_in_meters only.
 Design choice:  use LIMIT 5 to limit to 5 results
@@ -74,8 +57,6 @@ query_job.result()
 
 print("Query results loaded to the table {}".format(table_id))
 
-# can use QueryJob row iterator results to write tests later
-# now, to print out the table results so no need to check in BQ
 print("The query data:")
 for row in query_job:
     print(row)
